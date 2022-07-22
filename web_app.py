e4b9b048cfdcd03aba7500e98ccaed6bddf428e5
Prakhar-jais <67100593+Prakhar-jais@users.noreply.github.com>
2022-07-23 01:08:04 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -8,7 +8,7 @@ Created on Fri Jul 22 12:32:07 2022
 import numpy as np
 import pickle
 import streamlit as st 
-from streamlit_option_menu import option_menu
+import streamlit_option_menu as som
 
 
 from sklearn.preprocessing import StandardScaler
@@ -20,7 +20,7 @@ model2 = pickle.load(open(path2,'rb'))
 #creating np array# side bar for navigation
 
 with st.sidebar:
-    select = option_menu('AI Disease Detecting System',['Diabetes Detection','HeartDiseases Detection'],icons=  ['capsule','heart-pulse-fill'],default_index=0)
+    select = som.option_menu('AI Disease Detecting System',['Diabetes Detection','HeartDiseases Detection'],icons=  ['capsule','heart-pulse-fill'],default_index=0)
 
 
 if(select =='Diabetes Detection'):
